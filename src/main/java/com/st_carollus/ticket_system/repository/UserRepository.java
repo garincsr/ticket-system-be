fa75355a3e058b3a5f61ab3bc7da0dd4c49b6fa2
gaarincsr <garin@summitglobal.id>
2026-07-16 00:28:42 +0700

    update

--- src/main/java/com/st_carollus/ticket_system/repository/UserRepository.java
+++ src/main/java/com/st_carollus/ticket_system/repository/UserRepository.java
@@ -1,11 +1,17 @@
 package com.st_carollus.ticket_system.repository;
 
 import com.st_carollus.ticket_system.model.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+    Page<User> findByUsernameContainingIgnoreCaseOrEmailContainingIgnoreCaseOrFullNameContainingIgnoreCase(
+            String username, String email, String fullName, Pageable pageable);
+    List<User> findByRole_RoleCode(String roleCode);
 }
--- src/main/java/com/st_carollus/ticket_system/service/RoleService.java
+++ src/main/java/com/st_carollus/ticket_system/service/RoleService.java
@@ -2,6 +2,7 @@ package com.st_carollus.ticket_system.service;
 
 import com.st_carollus.ticket_system.model.dto.request.RoleRequest;
 import com.st_carollus.ticket_system.model.dto.response.RoleResponse;
+import com.st_carollus.ticket_system.model.entity.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -9,7 +10,7 @@ public interface RoleService {
     RoleResponse create(RoleRequest request);
     Page<RoleResponse> getAll(String search, Pageable pageable);
     RoleResponse getById(String id);
-    RoleResponse getByRoleCode(String roleCode);
+    Role getEntityByRoleCode(String roleCode);
     RoleResponse update(String id, RoleRequest request);
     void delete(String id);
 }
